bdbf70d08cd33d51297605abccd3ca4a02cdb3f6
Rafael Menon <rafael13rodrigo@gmail.com>
2020-08-29 13:29:10 -0300

    build production umbler

---
+++ build/precache-manifest.4a1635d59c18ba22b905ace46ee02ba5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7de162dac6440b444982ea6ea681253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c660da1e078c274998c2",
+    "url": "/static/css/main.9b361d89.chunk.css"
+  },
+  {
+    "revision": "35f12f53f4a38aad6716",
+    "url": "/static/js/2.54298f53.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.54298f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c660da1e078c274998c2",
+    "url": "/static/js/main.09669aa4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "0903622139839731fb5b9fdf06cf8c5e",
+    "url": "/static/media/navalha.09036221.svg"
+  },
+  {
+    "revision": "4d9c5a110e6ab14fb55a8b4fcb2f6ff1",
+    "url": "/static/media/secador.4d9c5a11.svg"
+  }
+]);
